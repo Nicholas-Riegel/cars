729bacf6247813236488b1c3982864c24d7d9393
Nicholas Riegel <nicholaspriegel@gmail.com>
2025-12-10 14:28:45 +0100

    Added jpa and postgres integration to pom.xml and application.properties.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,28 +3,28 @@ import axios, { AxiosError } from 'axios'
 
 function App() {
 
-  const [messageState, setMessageState] = useState('')
-  const [errorState, setErrorState] = useState<string | null>(null)
+	const [messageState, setMessageState] = useState('')
+	const [errorState, setErrorState] = useState<string | null>(null)
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const response = await axios.get('/api/home')
-        setMessageState(response.data)
-      } catch (err: unknown) {
-        setErrorState(err instanceof AxiosError ? err.message : 'An unknown error occurred')
-      }
-    })()
-  }, [])
+	useEffect(() => {
+		(async () => {
+			try {
+				const response = await axios.get('/api/home')
+				setMessageState(response.data)
+			} catch (err: unknown) {
+				setErrorState(err instanceof AxiosError ? err.message : 'An unknown error occurred')
+			}
+		})()
+	}, [])
 
-  return (
-    <>
-      {errorState 
-        ? <p>{errorState}</p> 
-        : <p>Backend says: {messageState}</p>
-      }
-    </>
-  )
+	return (
+		<>
+			{errorState 
+				? <p>{errorState}</p> 
+				: <p>Backend says: {messageState}</p>
+			}
+		</>
+	)
 }
 
 export default App
